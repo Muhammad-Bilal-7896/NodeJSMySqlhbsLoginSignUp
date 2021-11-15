dc743828c3495bbc0686cbfe12f6d1ba0dff9417
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-11-15 22:09:49 +0500

    feat

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const mysql = require("mysql");
 const dotenv = require("dotenv");
 
@@ -7,11 +8,17 @@ dotenv.config({path: './.env'})
 const app = express();
 
 const db = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
+    host:process.env.DATABASE_HOST,
+    user:process.env.DATABASE_USER,
+    password:process.env.DATABASE_PASSWORD,
     database:process.env.DATABASE
-})
+});
+
+const publicDirectory = path.join(__dirname,'./public');
+app.use(express.static(publicDirectory));
+
+console.log(__dirname);
+app.set('view engine','hbs');
 
 db.connect((error)=>{
     if(error){
@@ -22,7 +29,8 @@ db.connect((error)=>{
 })
 
 app.get("/",(req,res)=>{
-    res.send("<h1>Home Page</h1>");
+    //res.send("<h1>Home Page</h1>");
+    res.render("index")
 })
 
 app.listen(5000,()=>{
