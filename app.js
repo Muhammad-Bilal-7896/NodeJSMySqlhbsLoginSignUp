ff5dc467f42821f801a0d1556dc14506effd3943
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-11-15 21:23:19 +0500

    feat

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mysql = require("mysql");
 
 const app = express();
+
+const db = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'http://localhost/phpmyadmin/'
+})
 
 app.get("/",(req,res)=>{
     res.send("<h1>Home Page</h1>");
